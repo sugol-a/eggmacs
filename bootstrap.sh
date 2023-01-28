0f44d2421f548c36f1e4e68e658f8e813a63ff1e
al <alister.sanders@protonmail.com>
2023-01-28 13:01:31 +1300

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -xe
+
+die() {
+    echo -e "$@" >&2
+}
+
+require() {
+    which $1 >/dev/null || die "failed to find $1"
+}
+
+require git
+require emacs
+
+[[ -d ~/.emacs.d/ ]] && die "already a .emacs.d directory present."
+
+git clone https://github.com/sugol-a/eggmacs ~/.emacs.d
+exec emacs
