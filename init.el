a0f50358bc95ad2cf26d331d45fb75c36d3420f6
Alister Sanders <alister.sanders@cookietime.co.nz>
2023-09-01 13:44:58 +1200

    update

--- init.el
+++ init.el
@@ -3,7 +3,8 @@
 (setq user-full-name "Alister Sanders"
       user-mail-address "alister@sugol.org"
       custom-file "/dev/null"
-      backup-inhibited t)
+      backup-inhibited t
+      read-process-output-max (* 64 4096))
 
 (egg:package! benchmark-init
   :ensure t
@@ -143,6 +144,8 @@
     (iimage-mode +1))
   :hook t)
 
+;; svelte-mode uses stupid non-treesitter typescript mode
+(egg:package! typescript-mode)
 (egg:package! svelte-mode)
 
 (egg:package! lice
@@ -162,10 +165,10 @@
   
   :hook t)
 
-(egg:extend-mode! emacs-lisp-mode-hook
-  (progn
-    (company-mode))
-  :hook t)
+;;(egg:extend-mode! emacs-lisp-mode-hook
+;;  (progn
+;;    (company-mode))
+;;  :hook t)
 
 (defun al:typescript-indent-setup ()
   (indent-tabs-mode -1)
@@ -423,7 +426,7 @@
     ((al:æsthetic-map . "Commands that vaguely change the look of a frame/window/buffer")
      (al:lsp-map . "LSP Commands"))
   (:global
-   ("M-o" . #'other-window)
+   ("M-o" . #'ace-window)
    ("M-2" . #'split-window-vertically)
    ("M-3" . #'split-window-horizontally)
    ("M-0" . #'delete-window))
@@ -527,3 +530,4 @@
 	(glasses)))
 
 (egg:init)
+(put 'narrow-to-region 'disabled nil)
